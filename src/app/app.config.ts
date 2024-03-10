69d01a68e2968bb8c7f88749f371990700722661
ITTECHGROUP <desarrollo@it-techgroup.com>
2024-03-10 18:24:44 -0300

    Fix firebase keys

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,20 +7,14 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
+import * as firebaseConfig from '../../firebase-config.json';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(
       provideFirebaseApp(() =>
-        initializeApp({
-          projectId: 'bread-bitacora',
-          appId: '1:623223766552:web:f42e9e5c3b9ce6d8dccd6d',
-          storageBucket: 'bread-bitacora.appspot.com',
-          apiKey: 'AIzaSyD_rUlYlr7QZBJZtCZcaEPWIxkcE_67YW4',
-          authDomain: 'bread-bitacora.firebaseapp.com',
-          messagingSenderId: '623223766552',
-          measurementId: 'G-Z91SP3NLL4',
-        })
+        initializeApp(firebaseConfig)
       )
     ),
     importProvidersFrom(provideAuth(() => getAuth())),
